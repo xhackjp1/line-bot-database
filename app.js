24a2be686574b4ea3dfec9f2c9c2724283c6c7c8
松田 信介 <smatsuda@x-hack.jp>
2018-09-10 00:53:48 +0900

    データベースを使うサンプル

--- app.js
+++ app.js
@@ -18,30 +18,30 @@ var util = require('util');
 app.set('port', (process.env.PORT || 8000));
 // JSONの送信を許可
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 // JSONパーサー
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    res.send('<h1>hello world</h1>');
+  res.send('<h1>hello world</h1>');
 });
 
 // async.waterfall([function(){}], function(){})
 app.post('/callback', function(req, res) {
   async.waterfall([
-    function(callback) {
+      function(callback) {
         // リクエストがLINE Platformから送られてきたか確認する
         if (!validate_signature(req.headers['x-line-signature'], req.body)) {
-            return;
+          return;
         }
         // テキストか画像が送られてきた場合のみ返事をする
         if (
           (req.body['events'][0]['type'] != 'message') ||
           ((req.body['events'][0]['message']['type'] != 'text') &&
-          (req.body['events'][0]['message']['type'] != 'image'))
+            (req.body['events'][0]['message']['type'] != 'image'))
         ) {
-            return;
+          return;
         }
 
         // 特定の単語に反応させたい場合
@@ -56,115 +56,116 @@ app.post('/callback', function(req, res) {
         var message_type = req.body['events'][0]['message']['type'];
         var message_text = req.body['events'][0]['message']['text'];
         if (req.body['events'][0]['source']['type'] == 'user') {
-            request.get(getProfileOption(user_id), function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    callback(req, body['displayName'], message_id, message_type, message_text);
-                }
-            });
+          request.get(getProfileOption(user_id), function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+              callback(req, body['displayName'], message_id, message_type, message_text);
+            }
+          });
         }
-    },
-],
+      },
+    ],
 
-      function(req, displayName, message_id, message_type, message_text) {
+    function(req, displayName, message_id, message_type, message_text) {
 
-          databaseSample(req, message_text);
+      var message = "hello, " + displayName + "さん"; // helloと返事する
+      //var message = message_text; // おうむ返しする
+      //var message = message_text + "[" + message_text.length + "文字]";
 
-          //var message = "hello, " + displayName + "さん"; // helloと返事する
-          //var message = message_text; // おうむ返しする
-          //var message = message_text + "[" + message_text.length + "文字]";
+      sendMessage.send(req, [messageTemplate.textMessage(message)]);
 
-          //sendMessage.send(req, [messageTemplate.textMessage(message)]);
+      // データベースを使う
+      // databaseSample(req, message_text);
 
-          ///////////////////
-          // 画像で返事をする //
-          ///////////////////
-          /*
-          var messages = ["左上を押した", "右上を押した", "左下を押した", "右下を押した"];
-          if (message_text == "猫") {
-             sendMessage.send(req, [ messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/8cbL5dl.jpg') ]);
-             return;
-          } else if (message_text == "犬") {
-             sendMessage.send(req, [ messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/ph82KWH.jpg') ]);
-             return;
-          } else if (message_text == "鹿") {
-             sendMessage.send(req, [ messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/Z6ilhSI.jpg') ]);
-             return;
-          }
-          */
-          ///////////////////
-          // 画像で返事をする //
-          ///////////////////
-
-          //////////////////
-          // 天気APIパート //
-          /////////////////
-          /*
-          // 天気ときたら東京の天気が返ってくる
-          // APIキーの設定と、ライブラリの読み込みが必要
-          if (message_text === "天気") {
-            weather_api.weather(function (result) {
-              sendMessage.send(req, [ messageTemplate.textMessage(result) ]);
-              return;
-            });
-          // 天気　半角スペース　地名（ローマ字のみ　例：tokyo）でそこの天気が返ってくる
-          } else if (message_text.includes('天気')) {
-            const words = message_text.split(' ')
-            weather_api.weatherWithPlace(words[1], function (result) {
-              sendMessage.send(req, [ messageTemplate.textMessage(result) ]);
-              return;
-            });
-          } else {
-            sendMessage.send(req, [ messageTemplate.textMessage(message) ]);
-            return;
-          }
-          */
-          //////////////////
-          // 天気APIパート //
-          /////////////////
-
-          //////////////////
-          // 画像認識パート //
-          /////////////////
-          /*
-          if (message_type === 'image') {
-
-            // https://qiita.com/n0bisuke/items/17c795fea4c2b5571ce0
-            // 上のLINE Developersドキュメントのコードだとうまくいかない。
-            // chunkにresponseとbodyが一緒に入っている？
-            // encoding: nullが設定されてないから？
-            const options = {
-              url: `https://api.line.me/v2/bot/message/${message_id}/content`,
-              method: 'get',
-              headers: {
-                  'Authorization': 'Bearer ' + process.env.LINE_CHANNEL_ACCESS_TOKEN,
-              },
-              encoding: null
-            };
-
-            request(options, function(error, response, body) {
-              if (!error && response.statusCode == 200) {
-                console.log('Got responce');
-                visualRecognition.classify(body, function (result) {
-                  sendMessage.send(req, [ messageTemplate.textMessage(result) ]);
-                  return;
-                })
-              } else {
-                // @todo handle error
-              }
-            });
-          }
-          */
-          ////////////////////////
-          // 画像認識パートここまで //
-          ////////////////////////
-
-          return;
+      ///////////////////
+      // 画像で返事をする //
+      ///////////////////
+      /*
+      var messages = ["左上を押した", "右上を押した", "左下を押した", "右下を押した"];
+      if (message_text == "猫") {
+         sendMessage.send(req, [ messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/8cbL5dl.jpg') ]);
+         return;
+      } else if (message_text == "犬") {
+         sendMessage.send(req, [ messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/ph82KWH.jpg') ]);
+         return;
+      } else if (message_text == "鹿") {
+         sendMessage.send(req, [ messageTemplate.imagemapMessage(messages, 'https://i.imgur.com/Z6ilhSI.jpg') ]);
+         return;
       }
+      */
+      ///////////////////
+      // 画像で返事をする //
+      ///////////////////
+
+      //////////////////
+      // 天気APIパート //
+      /////////////////
+      /*
+      // 天気ときたら東京の天気が返ってくる
+      // APIキーの設定と、ライブラリの読み込みが必要
+      if (message_text === "天気") {
+        weather_api.weather(function (result) {
+          sendMessage.send(req, [ messageTemplate.textMessage(result) ]);
+          return;
+        });
+      // 天気　半角スペース　地名（ローマ字のみ　例：tokyo）でそこの天気が返ってくる
+      } else if (message_text.includes('天気')) {
+        const words = message_text.split(' ')
+        weather_api.weatherWithPlace(words[1], function (result) {
+          sendMessage.send(req, [ messageTemplate.textMessage(result) ]);
+          return;
+        });
+      } else {
+        sendMessage.send(req, [ messageTemplate.textMessage(message) ]);
+        return;
+      }
+      */
+      //////////////////
+      // 天気APIパート //
+      /////////////////
+
+      //////////////////
+      // 画像認識パート //
+      /////////////////
+      /*
+      if (message_type === 'image') {
+
+        // https://qiita.com/n0bisuke/items/17c795fea4c2b5571ce0
+        // 上のLINE Developersドキュメントのコードだとうまくいかない。
+        // chunkにresponseとbodyが一緒に入っている？
+        // encoding: nullが設定されてないから？
+        const options = {
+          url: `https://api.line.me/v2/bot/message/${message_id}/content`,
+          method: 'get',
+          headers: {
+              'Authorization': 'Bearer ' + process.env.LINE_CHANNEL_ACCESS_TOKEN,
+          },
+          encoding: null
+        };
+
+        request(options, function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log('Got responce');
+            visualRecognition.classify(body, function (result) {
+              sendMessage.send(req, [ messageTemplate.textMessage(result) ]);
+              return;
+            })
+          } else {
+            // @todo handle error
+          }
+        });
+      }
+      */
+      ////////////////////////
+      // 画像認識パートここまで //
+      ////////////////////////
+
+      return;
+    }
   );
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running');
+  console.log('Node app is running');
 });
 
 function databaseSample(req, sendword) {
@@ -178,9 +179,9 @@ function databaseSample(req, sendword) {
     let helpMessage3 = "'delete [削除するID]' と打つと 削除されます ID は 'list'で確認できます";
 
     sendMessage.send(req, [
-        messageTemplate.textMessage(helpMessage1),
-        messageTemplate.textMessage(helpMessage2),
-        messageTemplate.textMessage(helpMessage3)
+      messageTemplate.textMessage(helpMessage1),
+      messageTemplate.textMessage(helpMessage2),
+      messageTemplate.textMessage(helpMessage3)
     ]);
     return;
   }
@@ -230,8 +231,8 @@ function databaseSample(req, sendword) {
     console.log(result.rowCount);
 
     if (result.rowCount === 0) {
-        sendMessage.send(req, [messageTemplate.textMessage("データはありません")]);
-        return;
+      sendMessage.send(req, [messageTemplate.textMessage("データはありません")]);
+      return;
     }
     var randomId = getRandomInt(result.rowCount) - 1;
     console.log("検索ID" + randomId);
@@ -240,7 +241,6 @@ function databaseSample(req, sendword) {
     sendMessage.send(req, [messageTemplate.textMessage(r.word)]);
   });
 }
-
 
 // メッセージの長さを返す
 function textcount(body) {
@@ -258,12 +258,12 @@ function getProfileOption(user_id) {
     proxy: process.env.FIXIE_URL,
     json: true,
     headers: {
-        'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}'
+      'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}'
     }
   };
 }
 
 // 署名検証
 function validate_signature(signature, body) {
-    return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+  return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
 }
