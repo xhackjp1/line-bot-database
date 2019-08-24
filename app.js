c94e0f98b9ac744536fdc208f21786649ad13d84
Your Name <youremail@example.com>
2019-08-24 05:20:37 +0000

    update

--- app.js
+++ app.js
@@ -85,17 +85,10 @@ app.post('/callback', function(req, res) {
       //var message = message_text + "[" + message_text.length + "文字]";
       //sendMessage.send(req, [messageTemplate.textMessage(message_text)]);
       
-      var message = ""
-      if(message_text == "犬"){
-        message = "わんわん"
-      }
-      if(message_text == "猫"){
-        message = "にゃお"
-      }
-      if(message_text == "鹿"){
-        message = "ケーンケン"
-      }
-      sendMessage.send(req, [messageTemplate.textMessage(message)]);
+      var url = "https://i.imgur.com/I5AZqHV.png"
+      sendMessage.send(req, [
+        messageTemplate.imageMessage(url)
+      ]);
 
       // データベースを使う場合、下記のコードはコメントアウトしてください
       //sendMessage.send(req, [messageTemplate.textMessage(message), messageTemplate.quickMessage("質問に答えてね！")]);
